f9ee20cd7dd6fa0242858d38919cc1ea369c2155
SILUE HERMINE <herminesilue4@gmail.com>
2025-10-04 16:02:28 +0000

    correction

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-
-
 const express = require('express'); 
 const app = express(); 
-
 app.get('/', (req, res) => { 
 res.send('Hello Docker + CI/CD!'); 
 }); 
-
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./index.js')
 const PORT = 3000; 
-
 app.listen(PORT, () => { 
 console.log('Server running on port ${PORT}'); 
 });
