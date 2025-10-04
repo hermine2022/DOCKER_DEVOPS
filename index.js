8bc6ea88ff7cc9aeb0de7366d8bcc5244312727a
SILUE HERMINE <herminesilue4@gmail.com>
2025-10-04 15:39:33 +0000

    hemrine

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// index.js
-const app = require("./app");
-const PORT = 4000;
 
-// ✅ Définir la route avant de démarrer le serveur
-app.get("/", (req, res) => {
-  res.send("Hello Docker by hermine silue  + CI/CD!");
-});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const express = require('express'); 
+const app = express(); 
 
+app.get('/', (req, res) => { 
+res.send('Hello Docker + CI/CD!'); 
+}); 
+
+module.exports = app;
